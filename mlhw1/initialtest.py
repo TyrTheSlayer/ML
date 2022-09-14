a1991fdbb4b051b80cd709ba6d3bbbfe34c88f62
tyr <aedan.wells@student.nmt.edu>
2022-09-14 16:18:19 -0600

    develope averages

--- mlhw1/initialtest.py
+++ mlhw1/initialtest.py
@@ -16,4 +16,27 @@ np.random.shuffle(ind)
 X_train = df.iloc[ind[:int(0.7*df.shape[0])],:]
 X_test = df.iloc[ind[int(0.7*df.shape[0]):],:]
 
-print(df.size)
+#group by the class for easy parsing
+datasets = {}
+by_class = X_train.groupby('class')
+for groups, data in by_class:
+    datasets[groups] = data
+
+#get an average for each feature for each plant
+#versicolor
+versi_sl_avg = datasets['Iris-versicolor']['sepallength'].mean()
+versi_sw_avg = datasets['Iris-versicolor']['sepalwidth'].mean()
+versi_pw_avg = datasets['Iris-versicolor']['petalwidth'].mean()
+versi_pl_avg = datasets['Iris-versicolor']['sepallength'].mean()
+#setosa
+setos_sl_avg = datasets['Iris-setosa']['sepallength'].mean()
+setos_sw_avg = datasets['Iris-setosa']['sepalwidth'].mean()
+setos_pw_avg = datasets['Iris-setosa']['petalwidth'].mean()
+setos_pl_avg = datasets['Iris-setosa']['sepallength'].mean()
+#virginica
+virgi_sl_avg = datasets['Iris-virginica']['sepallength'].mean()
+virgi_sw_avg = datasets['Iris-virginica']['sepalwidth'].mean()
+virgi_pw_avg = datasets['Iris-virginica']['petalwidth'].mean()
+virgi_pl_avg = datasets['Iris-virginica']['sepallength'].mean()
+
+#For each data point in the training set, find the closest from the average templates. Use Manhattan, Euclidean and Cosine distances (note that higher cosine value means lower distance). Give the point that label. 
