465de0f6698a5a964aa661bbf045d16983f1f9de
tyr <aedan.wells@student.nmt.edu>
2022-09-15 09:52:40 -0600

    Major construction

--- mlhw1/q6_mnist.py
+++ mlhw1/q6_mnist.py
@@ -1,12 +1,30 @@
 from math import sqrt
-import pandas as pd
 import numpy as np
 from numpy.linalg import norm
+import pandas as pd
 import gzip
 import sys; 
 import pickle;
-from matplotlib import pyplot
 import numpy_indexed as npi
+
+def man_calculation(means, x_train):
+  total = 0
+  for i in range(28):
+    for j in range(28):
+      total += abs(means[i][j] - x_train[i][j])
+  return total
+
+def euc_calc(means, x_train):
+  total = 0
+  for i in range(28):
+    for j in range(28):
+      total += pow((means[i][j] - x_train[i][j]), 2)
+  return sqrt(total)
+
+def cos_calc(means, x_train):
+  A = np.array(means).flatten()
+  B = np.array(x_train).flatten()
+  return np.dot(A,B)/(norm(A)*norm(B))
 
 #add the mnist dataset
 f = gzip.open('mnist.pkl.gz', 'rb')
@@ -27,8 +45,65 @@ euc_test = np.zeros(10000, dtype = int)
 cos_train = np.zeros(60000, dtype = int)
 cos_test = np.zeros(10000, dtype = int)
 man_avg = np.zeros(10, dtype=int)
-
+euc_avg = np.zeros(10, dtype=int)
+cos_avg = np.zeros(10, dtype=int)
 #find distances
-for i in x_train.size:
-  #manhattan
-  
+for i in range(x_train.size):
+  for image in range(9):
+    #manhattan
+    man_avg[image] = man_calculation(means[image], x_train[i])
+    #euclidean
+    euc_avg[image] = euc_calc(means[image], x_train[i])
+    #cosine
+    cos_avg[image] = cos_calc(means[image], x_train[i])
+  #find the proper label as min from list
+  man_train[i] = man_avg.argmin()
+  euc_train[i] = euc_avg.argmin()
+  cos_train[i] = cos_avg.argmax()
+  #do the same with test set
+for i in range(x_test.size):
+  for image in range(9):
+    #manhattan
+    man_avg[image] = man_calculation(means[image], x_test[i])
+    #euclidean
+    euc_avg[image] = euc_calc(means[image], x_test[i])
+    #cosine
+    cos_avg[image] = cos_calc(means[image], x_test[i])
+  #find the proper label as min from list
+  man_test[i] = man_avg.argmin()
+  euc_test[i] = euc_avg.argmin()
+  cos_test[i] = cos_avg.argmax()
+
+#print training confusion matrices
+print("Training")
+print("Manhattan Confusion Matrix Training")
+confusion_matrix = pd.crosstab(y_train, man_train, rownames=['Actual'], colnames=['Predicted'])
+print(confusion_matrix)
+print("\n\n")
+
+print("Euclidean Confusion Matrix Training")
+confusion_matrix = pd.crosstab(y_train, euc_train, rownames=['Actual'], colnames=['Predicted'])
+print(confusion_matrix)
+print("\n\n")
+
+print("Cosine Confusion Matrix Training")
+confusion_matrix = pd.crosstab(y_train, cos_train, rownames=['Actual'], colnames=['Predicted'])
+print(confusion_matrix)
+print("\n\n")
+
+#print testing confusion matrices
+print("Testing")
+print("Manhattan Confusion Matrix Training")
+confusion_matrix = pd.crosstab(y_test, man_test, rownames=['Actual'], colnames=['Predicted'])
+print(confusion_matrix)
+print("\n\n")
+
+print("Euclidean Confusion Matrix Training")
+confusion_matrix = pd.crosstab(y_test, euc_test, rownames=['Actual'], colnames=['Predicted'])
+print(confusion_matrix)
+print("\n\n")
+
+print("Cosine Confusion Matrix Training")
+confusion_matrix = pd.crosstab(y_test, cos_test, rownames=['Actual'], colnames=['Predicted'])
+print(confusion_matrix)
+print("\n\n")
