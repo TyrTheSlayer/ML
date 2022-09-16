d92c36f2300c1b675ee72f18e3200a0d2c8ec66f
Sam <samuel.nix@student.nmt.edu>
2022-09-16 10:07:32 -0600

    hopefully finished kmeans clustering for iris

--- mlhw1/q7_iris.py
+++ mlhw1/q7_iris.py
@@ -5,6 +5,25 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def label_centroids(correct_labels, closest_centroid, k):
+    correct_centroid_labels = []
+    K = range(0, k)
+    for i in K:
+        tmp = []
+        for p in range(0,3):
+            tmp.append(0)
+        for j in range(0,len(closest_centroid)):
+            if (closest_centroid[j] == i):
+                tmp[correct_labels[j]] += 1
+        correct_centroid_labels.append(tmp.index(max(tmp)))
+    #print(correct_centroid_labels)
+    prediction = []
+    for j in range(0, len(closest_centroid)):
+        prediction.append(correct_centroid_labels[closest_centroid[j]])
+
+    return prediction
+
+
 # Loading data
 irisData = load_iris()
 
@@ -15,33 +34,46 @@ y = irisData.target
 # Split into training and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-K = range(1,16)
-J = range(0,15)
+K = range(1, 16)
+J = range(0, 15)
 distortions = []
 labels = []
 train_pred = []
 test_pred = []
-centroids =[]
+centroids = []
 
 for k in K:
-    kmeanModel = KMeans(n_clusters=k).fit(X_train,y_train)
+    kmeanModel = KMeans(n_clusters=k).fit(X_train, y_train)
     distortions.append(kmeanModel.inertia_)
     labels.append(kmeanModel.labels_)
     train_pred.append(kmeanModel.predict(X_train))
     test_pred.append(kmeanModel.predict(X_test))
     centroids.append(kmeanModel.cluster_centers_)
 
-for k in J:
-    print(k+1, end = "\n")
-    #print(centroids[k],end = "\n")
+#for k in J:
+    #print(k + 1, end="\n")
+    # print(centroids[k],end = "\n")
     #print(labels[k],end = "\n")
-    print(train_pred, end = "\n")
+     #print(train_pred, end = "\n")
+for k in K:
+    pred = label_centroids(y_train, train_pred[k-1], k)
+    print(k, end = " cluster ")
+    print("Training", end = "\n")
+    confusion_matrix = pd.crosstab(y_train, pred, rownames=['Actual'], colnames=['Predicted'])
+    print(confusion_matrix)
+    print("----------------------", end = "\n")
 
-print(y_train)
+    pred = label_centroids(y_test, test_pred[k - 1], k)
+    print(k, end = " cluster ")
+    print("Testing", end = "\n")
+    confusion_matrix = pd.crosstab(y_test, pred, rownames=['Actual'], colnames=['Predicted'])
+    print(confusion_matrix)
+    print("----------------------", end="\n")
+# print(y_train)
 
-#plt.figure()
-#plt.plot(K, distortions, 'bx-')
-#plt.xlabel('k')
-#plt.ylabel('Distortion')
-#plt.title('The Elbow Method showing the optimal k')
-#plt.show()
+# plt.figure()
+# plt.plot(K, distortions, 'bx-')
+# plt.xlabel('k')
+# plt.ylabel('Distortion')
+# plt.title('The Elbow Method showing the optimal k')
+# plt.show()
