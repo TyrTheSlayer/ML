6f4c69823b38f1f9ae9ff9db724df2eb48ca90a3
tyr <aedan.wells@student.nmt.edu>
2022-11-26 23:01:36 -0700

    Fix columns

--- project/formatting.py
+++ project/formatting.py
@@ -11,7 +11,7 @@ us_states = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado
 #columns for each 50 states, will be like each month
 length = [0]*50
 
-data = {"state": us_states, "m1": length, "m2": length, "m3": length, "m4": length, "m5": length, "m6": length, "m7": length, "m8": length, "m9": length, "m10": length, "m11": length, "m12": length}
+data = {"state": us_states, "mean-m1": length, "mean-m2": length, "mean-m3": length, "mean-m4": length, "mean-m5": length, "mean-m6": length, "mean-m7": length, "mean-m8": length, "mean-m9": length, "mean-m10": length, "mean-m11": length, "mean-m12": length}
 df1 = pd.DataFrame(data)
 
 
@@ -27,7 +27,7 @@ df1.to_csv("data/monthly-mean-temp.csv")
 #create mean pressure
 #! Pressure for Rhode Island and arkansas is all 0, 0 for some small ones (m6 for wyoming,
 #! m3 for oregon, m3 for north carolina, m9 for minnisota, and m4 for california)
-data = {"state": us_states, "m1": length, "m2": length, "m3": length, "m4": length, "m5": length, "m6": length, "m7": length, "m8": length, "m9": length, "m10": length, "m11": length, "m12": length}
+data = {"state": us_states, "press-m1": length, "press-m2": length, "press-m3": length, "press-m4": length, "press-m5": length, "press-m6": length, "press-m7": length, "press-m8": length, "press-m9": length, "press-m10": length, "press-m11": length, "press-m12": length}
 pressure = pd.DataFrame(data)
 #create mean temp here
 for file in monthly:
@@ -40,7 +40,7 @@ pressure.to_csv("data/monthly-station-pressure.csv")
 
 #Monthly Greatest Predipitation
 #!rhode island is missing precipitation
-data = {"state": us_states, "m1": length, "m2": length, "m3": length, "m4": length, "m5": length, "m6": length, "m7": length, "m8": length, "m9": length, "m10": length, "m11": length, "m12": length}
+data = {"state": us_states, "precip-m1": length, "precip-m2": length, "precip-m3": length, "precip-m4": length, "precip-m5": length, "precip-m6": length, "precip-m7": length, "precip-m8": length, "precip-m9": length, "precip-m10": length, "precip-m11": length, "precip-m12": length}
 precip = pd.DataFrame(data)
 #create mean temp here
 for file in monthly:
@@ -54,7 +54,7 @@ precip = precip.fillna(0)
 precip.to_csv("data/monthly-greatest-precip.csv")
 
 #maximum tempearture
-data = {"state": us_states, "m1": length, "m2": length, "m3": length, "m4": length, "m5": length, "m6": length, "m7": length, "m8": length, "m9": length, "m10": length, "m11": length, "m12": length}
+data = {"state": us_states, "max-m1": length, "max-m2": length, "max-m3": length, "max-m4": length, "max-m5": length, "max-m6": length, "max-m7": length, "max-m8": length, "max-m9": length, "max-m10": length, "max-m11": length, "max-m12": length}
 precip = pd.DataFrame(data)
 #create mean temp here
 for file in monthly:
@@ -67,7 +67,7 @@ precip = precip.fillna(0)
 precip.to_csv("data/monthly-max-temp.csv")
 
 #min tempearture
-data = {"state": us_states, "m1": length, "m2": length, "m3": length, "m4": length, "m5": length, "m6": length, "m7": length, "m8": length, "m9": length, "m10": length, "m11": length, "m12": length}
+data = {"state": us_states, "min-m1": length, "min-m2": length, "min-m3": length, "min-m4": length, "min-m5": length, "min-m6": length, "min-m7": length, "min-m8": length, "min-m9": length, "min-m10": length, "min-m11": length, "min-m12": length}
 precip = pd.DataFrame(data)
 #create mean temp here
 for file in monthly:
@@ -80,7 +80,7 @@ precip = precip.fillna(0)
 precip.to_csv("data/monthly-min-temp.csv")
 
 #total snowfall
-data = {"state": us_states, "m1": length, "m2": length, "m3": length, "m4": length, "m5": length, "m6": length, "m7": length, "m8": length, "m9": length, "m10": length, "m11": length, "m12": length}
+data = {"state": us_states, "snow-m1": length, "snow-m2": length, "snow-m3": length, "snow-m4": length, "snow-m5": length, "snow-m6": length, "snow-m7": length, "snow-m8": length, "snow-m9": length, "snow-m10": length, "snow-m11": length, "snow-m12": length}
 precip = pd.DataFrame(data)
 #create mean temp here
 for file in monthly:
