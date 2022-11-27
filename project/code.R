50ecc56fa8e9eaaf8d78336be9bec673392089fa
snix <samuel.nix@student.nmt.edu>
2022-11-26 21:36:02 -0700

    KMeans clustering works

--- project/code.R
+++ project/code.R
@@ -39,6 +39,63 @@ plot(pressure_tree, labels = pressure$state, main = "Pressure Dendrogram")
 snowfall_tree = hclust(dist(scale(snowfall[3:14])), "ave")
 plot(snowfall_tree, labels = snowfall$state, main = "snowfall Dendrogram")
 
+###############KMeans#################
+K = 5
+
+#temp
+km1 = kmeans(scale(temp[3:14]), K, nstart = 5)
+clust=km1$cluster
+for (i in 1:K) {
+  print(i)
+  print(temp$state[km1$cluster == i])
+}
+#table(temp$state, clust)
+
+#min temp
+km2 = kmeans(scale(min_temp[3:14]),K, nstart = 10)
+clust=km2$cluster
+#table(temp$state, clust)
+for (i in 1:K) {
+  print(i)
+  print(min_temp$state[km2$cluster == i])
+}
+
+#max temp
+km3 = kmeans(scale(max_temp[3:14]),K, nstart = 10)
+clust=km3$cluster
+#table(temp$state, clust)
+for (i in 1:K) {
+  print(i)
+  print(max_temp$state[km3$cluster == i])
+}
+
+#greatest precip
+km4 = kmeans(scale(greatest_precip[3:14]),K, nstart = 10)
+clust=km4$cluster
+#table(temp$state, clust)
+for (i in 1:K) {
+  print(i)
+  print(greatest_precip$state[km4$cluster == i])
+}
+
+#pressure
+km5 = kmeans(scale(pressure[3:14]),K, nstart = 10)
+clust=km5$cluster
+#table(temp$state, clust)
+for (i in 1:K) {
+  print(i)
+  print(pressure$state[km5$cluster == i])
+}
+
+#snowfall
+km6 = kmeans(scale(min_temp[3:14]),K, nstart = 10)
+clust=km6$cluster
+#table(temp$state, clust)
+for (i in 1:K) {
+  print(i)
+  print(pressure$state[km6$cluster == i])
+}
+
 ######################### NY Dataset code ####################
 #data = read.csv("Weather.csv")
 
